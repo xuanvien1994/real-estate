a5c211f8a0de89e4d3f25b1a7d5dd8fce8a24af7
xuanvien1994 <vien.xuan.tran@steadfasttech.com.au>
2018-06-04 16:58:44 +0700

    create service page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 router.get('/about', function(req, res){
   res.render('about', {
     title: 'About'
+  });
+});
+
+router.get('/service', function(req, res){
+  res.render('service', {
+    title: 'Service'
   });
 });
 
